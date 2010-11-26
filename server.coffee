a41a4c32f6c7e24a2f907918007d2d5b986b5a45
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-11-25 21:50:11 -0700

    working

---
+++ server.coffee
@@ -0,0 +1,87 @@
+require('joose')
+require('joosex-namespace-depended')
+require('hash')
+Url = require('url')
+Fs = require('fs')
+Http = require('http')
+Query = require('querystring')
+Sys = require('sys')
+Readability = require('readability')
+Spawn = require('child_process').spawn
+Request = require('request')
+Config = JSON.parse(Fs.readFileSync('config.json', 'utf8'))
+Postmark = require('./postmark')(Config.postmark, { ssl: true })
+Chain = require('./chain-gang').create({ workers: 1 })
+
+fourOhFour = (res) ->
+  res.writeHead(404, {
+    'Content-Length': 0
+  })
+  res.end()
+
+job = (url) ->
+  (worker) ->
+    Request { uri: url }, (error, response, body) ->
+      if error?
+        worker.finish()
+      else
+        try
+          Readability.parse body, url, (result) ->
+            filename = Hash.sha1(url)
+            Fs.writeFile "#{filename}.html", result.content, (err) ->
+              if err?
+                worker.finish()
+              else
+                wkhtmltopdf = Spawn('wkhtmltopdf', ['--page-size', 'letter', '--encoding', 'utf-8', "#{filename}.html", "#{filename}.pdf"])
+                wkhtmltopdf.on 'exit', (code) ->
+                  if 0 == code
+                    Fs.readFile "#{filename}.pdf", 'base64', (err, data) ->
+                      if err?
+                        Sys.puts("error reading file")
+                        worker.finish()
+                      else
+                        Postmark.send {
+                          From: Config.email.from,
+                          To: Config.email.to,
+                          Subject: 'convert',
+                          TextBody: 'Straight to your Kindle!',
+                          Attachments: [{
+                            Name: "#{result.title}.pdf",
+                            Content: data,
+                            ContentType: 'application/pdf'
+                          }]
+                        }, (error, response, body) ->
+                          switch response.statusCode
+                            when 401
+                              Sys.puts('Incorrect API key')
+                            when 422
+                              Sys.puts('Malformed request')
+                            when 200
+                              Sys.puts('Everything went smoothly')
+                            else
+                              Sys.puts('Some other stupid problem')
+                          Fs.unlink("#{filename}.pdf")
+                          Fs.unlink("#{filename}.html")
+                          worker.finish()
+                  else
+                    Sys.puts("wkhtmltopdf exited with code #{code}")
+                    worker.finish()
+        catch e
+          worker.finish()
+
+Http.createServer((req, res) ->
+  url = Url.parse(req.url)
+  if url.query?
+    query = Query.parse(url.query)
+    if query.u? && query.key == Config.key
+      url = query.u
+      res.writeHead(200, {
+        'Content-Length': 0
+      })
+      res.end()
+      Chain.add job(url)
+    else
+      fourOhFour(res)
+  else
+    fourOhFour(res)
+).listen(parseInt(process.ARGV[2] || '8080'));
---
+++ server.js
@@ -0,0 +1,116 @@
+var Chain, Config, Fs, Http, Postmark, Query, Readability, Request, Spawn, Sys, Url, fourOhFour, job;
+require('joose');
+require('joosex-namespace-depended');
+require('hash');
+Url = require('url');
+Fs = require('fs');
+Http = require('http');
+Query = require('querystring');
+Sys = require('sys');
+Readability = require('readability');
+Spawn = require('child_process').spawn;
+Request = require('request');
+Config = JSON.parse(Fs.readFileSync('config.json', 'utf8'));
+Postmark = require('./postmark')(Config.postmark, {
+  ssl: true
+});
+Chain = require('./chain-gang').create({
+  workers: 1
+});
+fourOhFour = function(res) {
+  res.writeHead(404, {
+    'Content-Length': 0
+  });
+  return res.end();
+};
+job = function(url) {
+  return function(worker) {
+    return Request({
+      uri: url
+    }, function(error, response, body) {
+      if (typeof error !== "undefined" && error !== null) {
+        return worker.finish();
+      } else {
+        try {
+          return Readability.parse(body, url, function(result) {
+            var filename;
+            filename = Hash.sha1(url);
+            return Fs.writeFile("" + (filename) + ".html", result.content, function(err) {
+              var wkhtmltopdf;
+              if (typeof err !== "undefined" && err !== null) {
+                return worker.finish();
+              } else {
+                wkhtmltopdf = Spawn('wkhtmltopdf', ['--page-size', 'letter', '--encoding', 'utf-8', ("" + (filename) + ".html"), ("" + (filename) + ".pdf")]);
+                return wkhtmltopdf.on('exit', function(code) {
+                  if (0 === code) {
+                    return Fs.readFile("" + (filename) + ".pdf", 'base64', function(err, data) {
+                      if (typeof err !== "undefined" && err !== null) {
+                        Sys.puts("error reading file");
+                        return worker.finish();
+                      } else {
+                        return Postmark.send({
+                          From: Config.email.from,
+                          To: Config.email.to,
+                          Subject: 'convert',
+                          TextBody: 'Straight to your Kindle!',
+                          Attachments: [
+                            {
+                              Name: ("" + (result.title) + ".pdf"),
+                              Content: data,
+                              ContentType: 'application/pdf'
+                            }
+                          ]
+                        }, function(error, response, body) {
+                          switch (response.statusCode) {
+                            case 401:
+                              Sys.puts('Incorrect API key');
+                              break;
+                            case 422:
+                              Sys.puts('Malformed request');
+                              break;
+                            case 200:
+                              Sys.puts('Everything went smoothly');
+                              break;
+                            default:
+                              Sys.puts('Some other stupid problem');
+                          }
+                          Fs.unlink("" + (filename) + ".pdf");
+                          Fs.unlink("" + (filename) + ".html");
+                          return worker.finish();
+                        });
+                      }
+                    });
+                  } else {
+                    Sys.puts("wkhtmltopdf exited with code " + (code));
+                    return worker.finish();
+                  }
+                });
+              }
+            });
+          });
+        } catch (e) {
+          return worker.finish();
+        }
+      }
+    });
+  };
+};
+Http.createServer(function(req, res) {
+  var _ref, query, url;
+  url = Url.parse(req.url);
+  if (typeof (_ref = url.query) !== "undefined" && _ref !== null) {
+    query = Query.parse(url.query);
+    if ((typeof (_ref = query.u) !== "undefined" && _ref !== null) && query.key === Config.key) {
+      url = query.u;
+      res.writeHead(200, {
+        'Content-Length': 0
+      });
+      res.end();
+      return Chain.add(job(url));
+    } else {
+      return fourOhFour(res);
+    }
+  } else {
+    return fourOhFour(res);
+  }
+}).listen(parseInt(process.ARGV[2] || '8080'));

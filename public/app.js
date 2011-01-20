e33fa0c29ab375101a20edf9dfd8ef89c1c13961
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-01-19 22:18:23 -0700

    Use the proper host

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $('#bookmarklet').html('Send to my Kindle!').attr('href', "javascript:(function() { \
       var script = document.createElement('script'); \
       script.type = 'text/javascript'; \
-      script.src = 'http://localhost:9090/bookmarklet.js?to=" + encodeURIComponent(this.value) + "&t=' + (new Date()).getTime(); \
+      script.src = 'http://kindlebility.darkhax.com/bookmarklet.js?to=" + encodeURIComponent(this.value) + "&t=' + (new Date()).getTime(); \
       document.getElementsByTagName('head')[0].appendChild(script); \
     })();");
   });

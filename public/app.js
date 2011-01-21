64576fa51e150ec5cd686d2bebce6d5b8a2a42b5
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-01-20 20:03:33 -0700

    Put the div in immediately. Closes #8 Link to better blog post. Closes #9 Update (though I don't think anything changed) vimeo embed. Closes #10

--- public/app.js
+++ public/app.js
@@ -5,6 +5,21 @@ $(document).ready(function() {
       scrollTop: $(document).height()
     });
     $('#bookmarklet').html('Send to my Kindle!').attr('href', "javascript:(function() { \
+      var setupDiv = function() { \
+        var id = 'kindlebility'; \
+        var div = document.getElementById(id); \
+        if (null == div) { \
+          div = document.createElement('div'); \
+          div.id = id; div.style.width = '200px'; div.style.height = '30px'; \
+          div.style.position = 'fixed'; div.style.top = '10px'; div.style.left = '10px'; \
+          div.style.background = 'white'; div.style.borderColor = 'black'; div.style.borderStyle = 'solid'; \
+          div.style.borderWidth = '2px'; div.style.zIndex = '99999999'; div.style.padding = '5px'; \
+          div.style.paddingTop = '16px'; div.style.textAlign = 'center'; \
+          div.innerText = 'Working...'; \
+          document.getElementsByTagName('body')[0].appendChild(div); \
+        } \
+      }; \
+      setupDiv(); \
       var script = document.createElement('script'); \
       script.type = 'text/javascript'; \
       script.src = 'http://kindlebility.darkhax.com/bookmarklet.js?to=" + encodeURIComponent(this.value) + "&t=' + (new Date()).getTime(); \
